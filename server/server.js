1bbe147f0675ce949710699861968a5aceac263f
Andres <arias.andres2321@gmail.com>
2024-04-24 14:36:42 -0400

    feature: save key doc to db

--- server/server.js
+++ server/server.js
@@ -1,28 +1,42 @@
 import express from "express";
 import { nanoid } from "nanoid";
+import { addDays, format } from "date-fns";
 
 import { insertDocument, isKeyInDb } from "../services/couchDBService.js";
 const app = express();
 const port = 3000;
-// import couchDBService from "../services/couchDBService";
+
 app.use(express.json());
 
 // shorten url
 const domain = "https://www.something.com";
+
 app.post("/shortify", (req, res) => {
-  const { url } = req.body;
-  const key = nanoid(8);
-  // console.log(nanoid(8));
-  // create key for original url
+  const { url, customKey, customExpireDate } = req.body;
+  
+  const generatedKey = nanoid(8);
+  const key = customKey || generatedKey;
+
+  const timestamp = new Date();
   const shortednedUrl = `${domain}/${key}`;
+  const date = customExpireDate || 15;
+  const expiryDate = addDays(timestamp, date);
+
+  const doc = {
+    _id: key,
+    originalUrl: url,
+    shortenedUrl: shortednedUrl,
+    timestamp: format(timestamp, "yyyy-MM-dd"),
+    expireDate: format(expiryDate, "yyyy-MM-dd"),
+  };
 
   // check db for existing matches from couchDBService
+  if (!isKeyInDb(key)) {
+    // save to db from couchDBService
+    insertDocument(doc);
+  }
 
-  // save to db from couchDBService
-  const test = insertDocument(key);
-  console.log(`test: `, test);
-  console.log(`test: `, test);
-  res.send(shortednedUrl);
+  res.send(doc);
 });
 
 // redirection of url
--- services/couchDBService.js
+++ services/couchDBService.js
@@ -3,12 +3,10 @@ import nano from "nano";
 const couch = nano("http://admin:Andresarias23@127.0.0.1:5984");
 
 const db = couch.db.use("testdb");
-async function insertDocument(key) {
+async function insertDocument(doc) {
   try {
-    const response = await db.insert({ _id: key, testeew: "test" });
-    if (response.ok) {
-      return response;
-    } else {
+    const response = await db.insert(doc);
+    if (!response.ok) {
       throw new Error("Insert operation failed");
     }
   } catch (error) {
@@ -19,12 +17,13 @@ async function insertDocument(key) {
 
 // async function destroFromDb(key) {}
 
-// async function destroFromDb(key) {}
-
 async function isKeyInDb(key) {
-  const response = await db.get(key, { revs_info: true });
-  //   console.log(response._id);
-  if (response._id) return true;
+  try {
+    const response = await db.get(key, { revs_info: true });
+    if (response._id) return true;
+  } catch (error) {
+    return false;
+  }
 }
 
 export { insertDocument, isKeyInDb };
